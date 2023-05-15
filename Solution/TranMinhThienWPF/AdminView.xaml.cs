1fb2bfcbfc38be89194e970a9afe736fc9ee08d0
Thientm-den <Denkhotieu@gmail.com>
2023-05-15 13:52:31 +0700

    admin view

---
+++ Solution/TranMinhThienWPF/AdminView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TranMinhThienWPF
+{
+    /// <summary>
+    /// Interaction logic for AdminView.xaml
+    /// </summary>
+    public partial class AdminView : Window
+    {
+        public AdminView()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClickUpdate(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
